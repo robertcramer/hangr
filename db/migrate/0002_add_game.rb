d6a6c65df4ccc5d5ccddcb9b24dcb4591a5eeaa4
robertcramer <robertbcramer@icloud.com>
2015-10-07 09:15:43 -0400

    fixed gameover to boolean

--- db/migrate/0002_add_game.rb
+++ db/migrate/0002_add_game.rb
@@ -2,7 +2,7 @@ class AddGame < ActiveRecord::Migration
   def up
     create_table :games do |t|
       t.string  :players
-      t.string  :game_over?
+      t.boolean  :game_over?
       t.integer :guesses_taken
       t.string  :answer
     end
